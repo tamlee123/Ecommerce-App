3fa32af3e744f985a8d6ec92a1025c5da6c4c279
tam <cherykim123@gmail.com>
2021-01-27 20:59:32 -0800

    adding loading: true in state to avoid getting error

--- frontend/src/reducers.js/productReducers.js
+++ frontend/src/reducers.js/productReducers.js
@@ -4,7 +4,10 @@ import {
   PRODUCT_LIST_FAIL,
 } from "../constants/productConstant";
 
-export const productListReducer = (state = { products: [] }, action) => {
+export const productListReducer = (
+  state = { loading: true, products: [] },
+  action
+) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true };
