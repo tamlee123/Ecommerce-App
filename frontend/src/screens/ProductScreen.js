357d8e4b543e8cff27d4dafbd7b718149487deb0
tam <cherykim123@gmail.com>
2021-01-27 22:41:44 -0800

    add redux to ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,72 +1,89 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import data from '../data';
-import Rating from './../components/Rating';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Rating from "./../components/Rating";
+import LoadingBox from "./../components/LoadingBox";
+import MessageBox from "./../components/MessageBox";
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-    const product = data.products.find(x => x._id === props.match.params.id);
-    if(!product) {
-        return <div>Product Not Found</div>;
-    }
-    return (
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
         <div>
-            <Link to="/">Back to Result</Link>
-            <div className="row top">
-                <div className="col-2">
-                    <img className="large" src={product.image} alt={product.name}></img>
-                </div>
-
-                <div className="col-1">
-                    <ul>
-                        <li>
-                            <h1>{product.name}</h1>
-                        </li>
-                        <li>
-                            <Rating
-                                rating={product.rating}
-                                numReviews={product.numReviews}
-                            ></Rating>
-                        </li>
-                        <li>Price: ${product.price}</li>
-                        <li>
-                            Description:
-                                <p>{product.description}</p>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="col-1">
-                    <div className="card card-body">
-                        <ul>
-                            <li>
-                                <div className="row">
-                                    <div>Price</div>
-                                    <div className="price">${product.price}</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="row">
-                                    <div>Staus</div>
-                                </div>
-                                <div>
-                                    {product.countInStock >0 ? (
-                                        <span className="success">In Stock</span>
-                                    ) : (
-                                        <span className="danger">Unavailable</span>
-                                    )
-                                    }
-                                </div>
-                            </li>
-                            <li>
-                                <button className="primary block">Add to Card</button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
+          <Link to="/">Back to Result</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img
+                className="large"
+                src={product.image}
+                alt={product.name}
+              ></img>
             </div>
+
+            <div className="col-1">
+              <ul>
+                <li>
+                  <h1>{product.name}</h1>
+                </li>
+                <li>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  ></Rating>
+                </li>
+                <li>Price: ${product.price}</li>
+                <li>
+                  Description:
+                  <p>{product.description}</p>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    <div className="row">
+                      <div>Price</div>
+                      <div className="price">${product.price}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Staus</div>
+                    </div>
+                    <div>
+                      {product.countInStock > 0 ? (
+                        <span className="success">In Stock</span>
+                      ) : (
+                        <span className="danger">Unavailable</span>
+                      )}
+                    </div>
+                  </li>
+                  <li>
+                    <button className="primary block">Add to Card</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default ProductScreen;
