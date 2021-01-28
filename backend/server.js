7893f588d16361610cb71d8529df6b092fe0b2a5
tam <cherykim123@gmail.com>
2021-01-27 22:44:18 -0800

    create API address for ProductScreen

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
-import express from 'express'; 
-import data from './data.js';
+import express from "express";
+import data from "./data.js";
 
-const app = express ();
+const app = express();
 
-app.get('/api/products', (req,res) => {
-    res.send(data.products)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
 
-app.get('/', (req,res) => {
-    res.send('Server is ready');
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`);
-})
+  console.log(`Server at http://localhost:${port}`);
+});
