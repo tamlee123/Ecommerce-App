174bbcfed07c4580705bed44eb59cbd51d9b76db
tam <cherykim123@gmail.com>
2021-01-29 18:47:45 -0800

    add cartReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,14 @@ import thunk from "redux-thunk";
 import {
   productDetailsReducer,
   productListReducer,
-} from "./reducers.js/productReducers";
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
