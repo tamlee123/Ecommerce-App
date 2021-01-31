53d45f9a6834a7eae11aa7c5be96600422762678
tam <cherykim123@gmail.com>
2021-01-30 19:51:02 -0800

    action deleteFrom

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstant";
+import { CART_ADD_ITEM, CART_DELETE_ITEM } from "../constants/cartConstant";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${productId}`);
@@ -13,6 +13,14 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
       product: data._id,
       qty,
     },
+  });
+  localStorage.setItem(`cartItems`, JSON.stringify(getState().cart.cartItems));
+};
+
+export const deleteFromCart = (productId) => (dispatch, getState) => {
+  dispatch({
+    type: CART_DELETE_ITEM,
+    payload: productId,
   });
   localStorage.setItem(`cartItems`, JSON.stringify(getState().cart.cartItems));
 };
