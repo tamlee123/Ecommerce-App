026c2e782f245c8146043f707e09f2e3c4da82c2
tam <cherykim123@gmail.com>
2021-02-01 19:52:49 -0800

    adding app.use productRouter

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
+import productRouter from "./routes/productRouter.js";
 import userRouter from "./routes/userRouter.js";
 
 const app = express();
@@ -10,28 +10,12 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/sleep-well", {
   useCreateIndex: true,
 });
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 app.use((err, rq, res, next) => {
   res.status(500).send({ message: err.message });
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/", (req, res) => {
-  res.send("Server is ready");
 });
 
 const port = process.env.PORT || 5000;
