4b459b38be11c53892a165adeef70531307523e9
tam <cherykim123@gmail.com>
2021-02-01 16:09:41 -0800

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
+mongoose.connect("mongodb://localhost/sleep-well", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.use((err, rq, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
