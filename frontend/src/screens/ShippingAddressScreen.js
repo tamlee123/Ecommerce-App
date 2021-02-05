610ad7fdd1cef9f9c5a9ec20a39b4d2616f5f04d
tam <cherykim123@gmail.com>
2021-02-05 13:56:40 -0800

    adding checkout steps on the front of the ShippingAddressScreen

---
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,11 @@
+import CheckoutSteps from "./../components/CheckoutSteps";
+
+function ShippingAddressScreen() {
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+    </div>
+  );
+}
+
+export default ShippingAddressScreen;
