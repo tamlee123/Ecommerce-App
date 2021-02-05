c3c4883b45539abbba1983c99ede386f25f44f61
tam <cherykim123@gmail.com>
2021-02-05 13:27:18 -0800

    edit redirect

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -8,29 +8,36 @@ import MessageBox from "../components/MessageBox";
 function SigninScreen(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   const redirect = props.location.search
     ? props.location.search.split("=")[1]
     : "/";
+
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo, loading, error } = userSignin;
+
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(signin(email, password));
   };
+
   useEffect(() => {
     if (userInfo) {
       props.history.push(redirect);
     }
   }, [props.history, redirect, userInfo]);
+
   return (
     <div>
       <form className="form" onSubmit={handleSubmit}>
         <div>
           <h1>Sign In</h1>
         </div>
+
         {loading && <LoadingBox></LoadingBox>}
         {error && <MessageBox variant="danger">{error}</MessageBox>}
+
         <div>
           <label htmlFor="email">Email address</label>
           <input
@@ -59,10 +66,14 @@ function SigninScreen(props) {
             Sign In
           </button>
         </div>
+
         <div>
           <label />
           <div>
-            New Customer? <Link to="/register">Create your account</Link>
+            New Customer?{" "}
+            <Link to={`/register?redirect=${redirect}`}>
+              Create your account
+            </Link>
           </div>
         </div>
       </form>
