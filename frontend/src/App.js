ff23b58f092246e167f2e48ce013cee3633a4cc0
tam <cherykim123@gmail.com>
2021-02-04 16:08:11 -0800

    adding Sign Out Link on the header of the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,19 @@ import ProductScreen from "./screens/ProductScreen";
 import { BrowserRouter, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
+import { signout } from "./actions/userActions";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const handleSignout = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -29,7 +34,16 @@ function App() {
               )}
             </Link>
             {userInfo ? (
-              <Link to="#">{userInfo.name}</Link>
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={handleSignout}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
             ) : (
               <Link to="/signin">Sign In</Link>
             )}
