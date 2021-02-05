a9733d8c1da494e0fba3761154de389c9eeaf7e4
tam <cherykim123@gmail.com>
2021-02-05 15:07:30 -0800

    save data

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -6,14 +6,16 @@ import { saveShippingAddress } from "./../actions/cartActions";
 function ShippingAddressScreen(props) {
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
   if (!userInfo) {
     props.history.push("./signin");
   }
-  const [fullName, setFullName] = useState();
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+  const [fullName, setFullName] = useState(shippingAddress.fullName);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
 
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
