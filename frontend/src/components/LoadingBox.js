6d65579b5a1cae5119eebb92d6e185c79ced8b17
tam <cherykim123@gmail.com>
2021-02-05 13:30:21 -0800

    adding class loading

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function LoadingBox(props) {
-    return (
-        <div>
-            <i className="fa fa-spinner fa-spin"></i> Loading...
-        </div>
-    );
+  return (
+    <div className="loading">
+      <i className="fa fa-spinner fa-spin"></i> Loading...
+    </div>
+  );
 }
 
 export default LoadingBox;
