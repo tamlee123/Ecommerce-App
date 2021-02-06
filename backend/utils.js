d8032fa7f5c82b23445e1fbcb6c72cb301ce07a9
tam <cherykim123@gmail.com>
2021-02-06 15:37:25 -0800

    adding middleware isAuth to authenticate request

--- backend/utils.js
+++ backend/utils.js
@@ -16,9 +16,9 @@ export const generateToken = (user) => {
 };
 
 export const isAuth = (req, res, next) => {
-  const authorization = req.header.authorization;
+  const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length);
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(
       token,
       process.env.JWT_SECRET || "somethingsecret",
