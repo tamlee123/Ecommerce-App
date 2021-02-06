1ca879d8719bd5377ad7968d9d427515046f1c47
tam <cherykim123@gmail.com>
2021-02-05 22:16:02 -0800

    adding PlaceOrderScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { signout } from "./actions/userActions";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -61,6 +62,7 @@ function App() {
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
