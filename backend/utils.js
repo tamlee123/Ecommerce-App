b94f5a0b0f434b608fb1321fc2d921007456136b
tam <cherykim123@gmail.com>
2021-02-06 00:49:00 -0800

    create authorization for user to post order

--- backend/utils.js
+++ backend/utils.js
@@ -8,9 +8,30 @@ export const generateToken = (user) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    process.env.JWT_SECRET || 'somethingsecret',
+    process.env.JWT_SECRET || "somethingsecret",
     {
       expiresIn: "30d",
     }
   );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.header.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "somethingsecret",
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: "Invalid Token" });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: "No Token Provided" });
+  }
 };
