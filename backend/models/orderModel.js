6f4a9adaf78e691df6aefada2da9ff545f1cf514
tam <cherykim123@gmail.com>
2021-02-06 15:22:50 -0800

    correct typo for shippingAddress

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -15,7 +15,7 @@ const orderSchema = new mongoose.Schema(
         },
       },
     ],
-    shippinngAddress: {
+    shippingAddress: {
       fullName: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
