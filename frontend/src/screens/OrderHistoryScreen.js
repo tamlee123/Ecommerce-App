73a4fe3ef6eb63bd71364771ba20ec6685bbd253
tam <cherykim123@gmail.com>
2021-02-08 13:35:09 -0800

    apply redux tasks using useSelector and useEffect

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { listMyOrder } from "../actions/orderActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 
 function OrderHistoryScreen(props) {
   const orderMyList = useSelector((state) => state.orderMyList);
   const { loading, error, orders } = orderMyList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listMyOrder());
+  }, [dispatch]);
   return (
     <div>
       <hi1>Order History</hi1>
@@ -28,9 +33,10 @@ function OrderHistoryScreen(props) {
           <tbody>
             {orders.map((order) => (
               <tr key={order._id}>
+                <td>{order._id}</td>
                 <td>{order.createdAt.substring(0, 10)}</td>
-                <td>{order.totalPrice}</td>
-                <td>{order._isPaid ? order.paidAt.substring(1, 10) : "No"}</td>
+                <td>{order.totalPrice.toFixed(2)}</td>
+                <td>{order.isPaid ? order.paidAt.substring(0, 10) : "No"}</td>
                 <td>
                   {order.isDelivered
                     ? order.deliveredAt.substring(0, 10)
