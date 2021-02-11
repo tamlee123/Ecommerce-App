35f499a24b749b29234e0e029f5d3ad10a486765
tam <cherykim123@gmail.com>
2021-02-11 14:13:07 -0800

    remove emtpy product array

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -27,10 +27,7 @@ export const productListReducer = (
   }
 };
 
-export const productDetailsReducer = (
-  state = { product: {}, payload: true },
-  action
-) => {
+export const productDetailsReducer = (state = { payload: true }, action) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return { loading: true };
