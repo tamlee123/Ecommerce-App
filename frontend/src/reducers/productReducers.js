422f28b58353ab238ab34df61dfca5fd6fe10ba7
tam <cherykim123@gmail.com>
2021-02-18 00:50:39 -0800

    edit screen commit

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -13,6 +13,9 @@ import {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from "../constants/productConstant";
 
 export const productListReducer = (
@@ -69,6 +72,19 @@ export const productUpdateReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case PRODUCT_UPDATE_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true };
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_DELETE_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
